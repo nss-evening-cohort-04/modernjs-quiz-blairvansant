ada8b720b66236480143f691da83a31434a44b8e
blairvansant <blairbot@gmail.com>
2016-11-07 18:07:40 -0600

    fixed the create new player function and got the submit btn working

--- javascripts/battle.js
+++ javascripts/battle.js
@@ -1,17 +1,18 @@
 $(document).ready(function(){
 
-  const showSelection = $("#submitResult");  
-  const outputDiv = $("#output");
-  const submitBtn = $("#submit");
-  const attackBtn = $("#attack");
+  let showSelection = $("#submitResult");  
+  let outputDiv = $("#output");
+  let submitBtn = $("#submit");
+  let attackBtn = $("#attack");
   let robot1;
   let robot2;
   let name1;
   let name2;
-  const select1 = $("#selectOne");
-  const select2 = $("#selectTwo");
+  let select1 = $("#selectOne");
+  let select2 = $("#selectTwo");
 
-  submitBtn.click();
+  submitBtn.click(newPlayer);
+  console.log("submit", submit);
   attackBtn.click();
 
 function newPlayer () {
@@ -21,8 +22,17 @@ function newPlayer () {
   robot1 = new window[select1.val()]();
   robot2 = new window[select2.val()]();
 
+$("#submit").on("click", function(){
+  console.log("button works");
+})
+
      
 let output = `<p>${name1} is the ${player1.name} robot and starts with ${player1.health} health and ${player1.damage} damage.</p>`;
   output += `<p>${name2} is the ${player2.name} robot and starts with ${player2.health} health and ${player2.damage} damage.</p>`;
   showSelection.append(output);
-};
+ };
+});
+
+
+
+
